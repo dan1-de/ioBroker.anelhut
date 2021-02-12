3942e6112a7759ab4423ff8407765b307ba0775b
Daniel Wölfel <registrierung.dani@online.de>
2021-02-12 20:51:00 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"anelhut adapter settings": {
+		en: "Adapter settings for anelhut",
+		de: "Adaptereinstellungen für anelhut",
+		ru: "Настройки адаптера для anelhut",
+		pt: "Configurações do adaptador para anelhut",
+		nl: "Adapterinstellingen voor anelhut",
+		fr: "Paramètres d'adaptateur pour anelhut",
+		it: "Impostazioni dell'adattatore per anelhut",
+		es: "Ajustes del adaptador para anelhut",
+		pl: "Ustawienia adaptera dla anelhut",
+		"zh-cn": "anelhut的适配器设置",
+	},
+	option1: {
+		en: "option1",
+		de: "Option 1",
+		ru: "Опция 1",
+		pt: "Opção 1",
+		nl: "Optie 1",
+		fr: "Option 1",
+		it: "opzione 1",
+		es: "Opción 1",
+		pl: "opcja 1",
+		"zh-cn": "选项1",
+	},
+	option2: {
+		en: "option2",
+		de: "Option 2",
+		ru: "option2",
+		pt: "opção 2",
+		nl: "Optie 2",
+		fr: "Option 2",
+		it: "opzione 2",
+		es: "opcion 2",
+		pl: "Opcja 2",
+		"zh-cn": "选项2",
+	},
+};
