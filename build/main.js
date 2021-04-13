e642b66d8e0354576dab209358dcee1d28ca430d
dan1-de <dan1-de@gmx.de>
2021-04-13 19:54:36 +0200

    test io

--- build/main.js
+++ build/main.js
@@ -267,6 +267,7 @@ class Anelhut extends utils.Adapter {
         const slotNumber = Number(idParts[4]);
         const status = idParts[5];
         if (type == "relais" && status == "Status") {
+            this.log.info("Relais switch command");
             this.anelConfigDevices.forEach((device) => {
                 if (device.DeviceName == hutName) {
                     device.HutCommunication.SwitchRelais(slotNumber, state, XorEncryptUserPass);
@@ -274,6 +275,7 @@ class Anelhut extends utils.Adapter {
             });
         }
         if (type == "io" && status == "Status") {
+            this.log.info("IO switch command");
             this.anelConfigDevices.forEach((device) => {
                 if (device.DeviceName == hutName) {
                     device.HutCommunication.SwitchIo(slotNumber, state, XorEncryptUserPass);
